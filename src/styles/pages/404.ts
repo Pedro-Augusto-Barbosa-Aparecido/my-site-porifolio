2b3e04a09285c5ab72b809e82e0ace7fcbccbdf1
Pedro-Augusto-Barbosa-Aparecido <77130441+Pedro-Augusto-Barbosa-Aparecido@users.noreply.github.com>
2022-11-13 18:43:58 -0300

    init 404 page

---
+++ src/styles/pages/404.ts
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import { styled } from "..";
+
+export const ImageBG = styled(Image, {});
+
+export const Container404 = styled("main", {
+  height: "100vh",
+  width: "100%",
+});
